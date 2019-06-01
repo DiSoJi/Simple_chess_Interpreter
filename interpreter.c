4b3efb3950f7430111d190976f4bfa9c9fbd5dc2
DiSoJi <diegosolisj@gmail.com>
2019-05-31 20:27:54 -0600

    Conected to library

--- interpreter.c
+++ interpreter.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include "../library/finger.h"
 
 void lower_string(char s[]) {
    int c = 0;
@@ -14,31 +15,12 @@ void lower_string(char s[]) {
       c++;
    }
 }
-/*
-void clean_token(char s[]){
-    int c = 0;
-   
-    while (s[c] != '\0') {
-      if ((s[c] == '(' ) || (s[c] == ')') || (s[c] == ',')) {
-        s[c] = ' ';
-      }
-      else
-      {
-        c++;
-      }
-      
-   }
 
-}*/
 int checktoken(char s[]){
-    //printf("Token in checktoken:%s \n",s);
-    //printf("Last char %c \n",s[5]);
     if (s[0] != '('){ 
-        //printf ("s[0] %c \n", s[0]);
         return 0;
         }
     else if (s[4] != ')') {
-        //printf ("s[-1] %c \n", s[4]);
         return 0;
     }else if (s[5] != '\n') return 2;
     else return 1;
@@ -61,59 +43,59 @@ int main(int argc, char *argv[]){
         printf("Error: Wrong option input (write -c)\n");
         return 1;
     }
-    printf("Processing: STARTED\n");
+    //set_device("/dev/ttyardu0");
+   // printf("Device Set\n");
+    //printf("Processing: STARTED\n");
     int allgood = 1;
     char *line_buf = NULL;
     char * token = (char*) malloc(2 * sizeof(char*));
+    char * prev_token = (char*) malloc(2 * sizeof(char*));
+    int x = 0;
+    int y = 0;
     size_t line_buf_size = 0;
     int line_count = 0;
     ssize_t line_size;
     const char s[2] = " ";
     /* Open the file for reading */
     FILE *fp = fopen(argv[2], "r");
-    FILE *outputfile = fopen("output_code", "w");
-    fclose(outputfile);
-    outputfile = fopen("output_code", "a");
+    //FILE *outputfile = fopen("output_code", "w");
+    //fclose(outputfile);
+    //outputfile = fopen("output_code", "a");
     if (!fp)
     {
         fprintf(stderr, "Error opening file '%s'\n", argv[2]);
         fclose(fp);
-        fclose(outputfile);
+        //fclose(outputfile);
         return 1;
     }
-
-    /* Get the first line of the file. */
-    //line_size = getline(&line_buf, &line_buf_size, fp);
-
+    set_device("/dev/ttyardu0");
+    printf("Device Set\n");
+    printf("Processing: STARTED\n");
     /* Loop through until we are done with the file. */
     while (line_size >= 0)
     {
-        //printf("Enter while\n");
-        /* Show the line details */
-        //printf("line[%06d]: chars=%06zd, buf size=%06zu, contents: %s", line_count,
-        //    line_size, line_buf_size, line_buf);
-        
+
         /* Get the next line */
         line_size = getline(&line_buf, &line_buf_size, fp);
         if (line_size <0){
             break;
         }
-        //printf("After get line\n");
         token = strtok(line_buf, s);
-        //printf("After tokenizing\n");
         lower_string(token);
-        //clean_token(token);
-        //printf("Token: %s \n",token);
-        if (strcmp(token,"pick\n") == 0 || strcmp(token,"drop\n") == 0){
-            fprintf(outputfile, "%s", token);
-            //printf("Found pick or drop \n");
+        
+        if (strcmp(token,"pick\n") == 0){
+            //printf("Found pick\n");
+            pick();
+            //wait(1);
+        }else if(strcmp(token,"drop\n") == 0){
+            //printf("Found drop\n");
+            drop();
+            //wait(1);
         }else if (strcmp(token,"move") == 0 || strcmp(token,"move&pick") == 0 || strcmp(token,"move&drop") == 0) {
-            fprintf(outputfile, "%s", token);
-            //printf("Found move \n");
+            
+            //fprintf(outputfile, "%s", token);
+            prev_token = token;
             token = strtok(NULL, s);
-            /*printf( "This is the token: %s\n", token );
-            int Tokensize = strlen(token);
-            printf("This is Tokensize: %i \n",Tokensize );*/
             if (checktoken(token) != 1){
                 if (checktoken(token) == 2){
                     printf("Syntax Error: Wrong Instruction ending in line  %i . All Instructions must end with line jump (not space or anything else)\n",line_count+1);    
@@ -134,9 +116,34 @@ int main(int argc, char *argv[]){
                 allgood = 0;
                 break;
             }else {
-                fprintf(outputfile, "%s", " ");
-                fprintf(outputfile, "%s", token);
-                //printf("Valid line \n");
+                //printf("Device Set\n");
+                x = atoi(&token[1]);
+                y = atoi(&token[3]);
+                x++;
+                y++;
+                //printf("x: %i\n",x);
+                //printf("y: %i\n",y);
+                //printf("Line Number: %i\n",line_count);
+                //printf("prev_token: %s\n",prev_token);
+                if (strcmp(prev_token,"move") == 0) {
+                    //printf("Found move\n");
+                    move(x,y);
+                    //wait(1);
+                }else if (strcmp(prev_token,"move&pick") == 0){
+                    //printf("Found move&pick\n");
+                    move(x,y);
+                    //wait(1);
+                    pick();
+                    //wait(1);
+                } else if (strcmp(prev_token,"move&drop") == 0) {
+                    //printf("Found move&drop\n");
+                    move(x,y);
+                    //wait(1);
+                    drop();
+                    //wait(1);
+                }
+                //fprintf(outputfile, "%s", " ");
+                //fprintf(outputfile, "%s", token);
             }
 
         }else {
@@ -160,7 +167,7 @@ int main(int argc, char *argv[]){
 
     /* Close the file now that we are done with it */
     fclose(fp);
-    fclose(outputfile);
+    //fclose(outputfile);
 
     return 0;
 }
